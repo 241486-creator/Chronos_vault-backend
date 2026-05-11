40533ebdaffbb37c5aecdfa47b15ea0cd4501a95
241486-creator <241486@students.au.edu.pk>
2026-05-12 02:51:39 +0500

    fix: force redeploy

--- backend/server.js
+++ backend/server.js
@@ -146,4 +146,4 @@ app.get('/get-vault/:user_id', async (req, res) => {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-module.exports = app;
+module.exports = app; 
