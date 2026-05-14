b80700d0e9c97804dd2776fd929aaf9a160523df
241486-creator <241486@students.au.edu.pk>
2026-05-15 03:21:26 +0500

    fix: remove options wildcard

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(cors({
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
 }));
-app.options('/(.*)', cors());
+app.use(cors());
 
 // ==========================================
 // DATABASE
